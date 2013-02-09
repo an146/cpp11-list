b5ffeb1d08fa7362ac96eefe4cef4b459e175eb7
Anton Novikov <an146@ya.ru>
2013-02-09 06:09:40 +0600

    some assertions refactoring

--- test_list.cpp
+++ test_list.cpp
@@ -8,7 +8,7 @@ using ourstl::list;
 
 template <class InputIterator1, class InputIterator2>
 void
-assert_equal_ranges(InputIterator1 b1, InputIterator1 e1, InputIterator2 b2, InputIterator2 e2)
+assert_range(InputIterator1 b1, InputIterator1 e1, InputIterator2 b2, InputIterator2 e2)
 {
     int pos = 0;
     for (; b1 != e1 && b2 != e2; ++b1, ++b2, ++pos) {
@@ -28,12 +28,21 @@ assert_equal_ranges(InputIterator1 b1, InputIterator1 e1, InputIterator2 b2, Inp
     /* ok, ranges are equal */
 }
 
+template <class InputIterator, class Cont>
+void assert_range(InputIterator b, InputIterator e, const Cont &c)
+{ return assert_range(b, e, c.begin(), c.end()); }
+
 template <class InputIterator, class T>
-void
-assert_range(InputIterator b, InputIterator e, std::initializer_list<T> expect)
-{
-    assert_equal_ranges(b, e, expect.begin(), expect.end());
-}
+void assert_range(InputIterator b, InputIterator e, std::initializer_list<T> il)
+{ return assert_range(b, e, il.begin(), il.end()); }
+
+template <class Cont1, class Cont2>
+void assert_cont(const Cont1 &c1, const Cont2 &c2)
+{ return assert_range(c1.begin(), c1.end(), c2.begin(), c2.end()); }
+
+template <class Cont, class T>
+void assert_cont(const Cont &c, std::initializer_list<T> il)
+{ return assert_range(c.begin(), c.end(), il.begin(), il.end()); }
 
 void
 test_ints()
@@ -46,23 +55,24 @@ test_ints()
     ls.push_back(2);
     ls.push_front(3);
     ls.push_back(4);
-    assert_range(ls.begin(), ls.end(), {3, 0, 1, 2, 4});
+    assert_cont(ls, {3, 0, 1, 2, 4});
 
     it = ls.begin(); ++it; ++it;
     ls.insert(it, 123);
-    assert_range(ls.begin(), ls.end(), {3, 0, 123, 1, 2, 4});
+    assert_cont(ls, {3, 0, 123, 1, 2, 4});
     it = ls.erase(it);
-    assert_range(ls.begin(), ls.end(), {3, 0, 123, 2, 4});
+    assert_cont(ls, {3, 0, 123, 2, 4});
     ls.insert(it, 124);
-    assert_range(ls.begin(), ls.end(), {3, 0, 123, 124, 2, 4});
+    assert_cont(ls, {3, 0, 123, 124, 2, 4});
 
     ls.reverse();
-    assert_range(ls.begin(), ls.end(), {4, 2, 124, 123, 0, 3});
+    assert_cont(ls, {4, 2, 124, 123, 0, 3});
 
     //some const_iterator testing
     const list<int> &cls = ls;
-    assert_range(cls.begin(), cls.end(), {4, 2, 124, 123, 0, 3});
-    assert_equal_ranges(ls.begin(), ls.end(), cls.begin(), cls.end());
+    assert_cont(cls, {4, 2, 124, 123, 0, 3});
+    assert_cont(ls, cls);
+    assert_cont(cls, ls);
     assert(ls.begin() == cls.begin());
     assert(cls.end() == ls.end());
     assert(ls.begin() != cls.end());
